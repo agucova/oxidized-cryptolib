e1cd5ceb4d49f4997c194cc34f347cfdb3fda553
Agustín Covarrubias <agucova@uc.cl>
2024-08-03 20:44:52 -0400

    docs: small comment edit

--- src/lib/master_key.rs
+++ src/lib/master_key.rs
@@ -9,7 +9,6 @@ pub struct MasterKey {
 impl MasterKey {
     #![allow(dead_code)]
     pub fn raw_key(&self) -> Vec<u8>{
-        // Combine the AES and MAC keys into a single key through copying
         [self.aes_master_key, self.mac_master_key].concat()
     }
 }
